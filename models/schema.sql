de71d58534322dbac919e873a2617304d6708e44
Vader55 <38086201+barb03@users.noreply.github.com>
2018-12-13 11:41:18 -0600

    added initial database schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,46 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS goal_db;
+CREATE DATABASE goal_db;
+USE goal_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE teams(
+id int NOT NULL AUTO_INCREMENT,
+user_team VARCHAR(255),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE categories(
+id int NOT NULL AUTO_INCREMENT,
+category_name VARCHAR(255),
+PRIMARY KEY (id)
+);
+INSERT INTO categories (category_name) VALUES ("fitness");
+INSERT INTO categories (category_name) VALUES ("finance");
+INSERT INTO categories (category_name) VALUES ("education");
+INSERT INTO categories (category_name) VALUES ("books");
+INSERT INTO categories (category_name) VALUES ("health");
+INSERT INTO categories (category_name) VALUES ("spiritual");
+INSERT INTO categories (category_name) VALUES ("career");
+INSERT INTO categories (category_name) VALUES ("diet");
+
+CREATE TABLE user_goal(
+id int NOT NULL AUTO_INCREMENT,
+goal_name VARCHAR(255),
+user_team INT NULL,
+FOREIGN KEY(user_team) REFERENCES teams(id),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+id int NOT NULL AUTO_INCREMENT,
+user_name VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+category_name INT NULL,
+goal VARCHAR(255),
+start_date date,
+goal_status VARCHAR(255),
+end_date date,
+user_goal INT NULL,
+FOREIGN KEY(category_name) REFERENCES categories(id),
+FOREIGN KEY(user_goal) REFERENCES user_goal(id),
+PRIMARY KEY (id)
+);
