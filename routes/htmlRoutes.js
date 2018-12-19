d8fd1129e4455571f3b3f05288b1056e397c57e6
greenman <caasilemroh@gmail.com>
2018-12-19 01:49:17 -0600

    Still chugging away at the keyboard

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 var db = require("../models");
 
+const consoleTable = require("console.table");
+
 module.exports = function(app) { 
   // Load index page
   app.get("/", function(req, res) {
@@ -33,6 +35,48 @@ module.exports = function(app) {
 
   app.get("/login", function(req, res) {
     res.render("login", {});
+  });
+  
+  app.get("/view-goal/:id", function(req, res){
+    db.goals.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results){
+      
+      var status = results.dataValues.completion;
+      var readableStatus = "";
+      if(status == 0){
+        //Not started
+        readableStatus = "This goal has not been started yet.";
+      }else if(status == 1){
+        //In progress
+        readableStatus = "This goal is already in progress.";
+      }else if(status == 2){
+        //Finished
+        readableStatus = "This goal has been successfully completed.";
+      }else if(status == 3){
+        //Abandoned
+        readableStatus = "This goal has been regretfuly abandoned =(";
+      }else{
+        //Cthulu has awoken from his eternal slumber
+        readableStatus = "This goal is misbehaving and needs to be exercised! Please ignore it for now.";
+      }
+      db.users.findOne({
+        where: {
+          id: results.dataValues.owner
+        }
+      }).then(function(results2){
+        console.table(results2.dataValues);
+        res.render("viewGoal", {
+          goal: results.dataValues, 
+          readableStatus: readableStatus, 
+          owner: {username: results2.dataValues.username}
+        });
+      });
+      
+    });
+    
   });
   
   // Render 404 page for any unmatched routes
